caaa36d370207f96f3d41b37b3090b173f54c5dc
Mihaly Sisak <sisak.mihaly@gmail.com>
2025-09-10 23:47:52 +0200

    cleanup 2

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 clear
-rm -rf _build
+#rm -rf _build || true
 cmake -G Ninja -B _build -S . -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build _build --config RelWithDebInfo
 ./_build/luau_example example.lua
