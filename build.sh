47c958e996b6a2364a3a30f350f2a5f8c7458329
Mihaly Sisak <sisak.mihaly@gmail.com>
2025-09-20 20:16:17 +0200

    do not build THDiskFile and THMemoryFile

--- build.sh
+++ build.sh
@@ -2,8 +2,12 @@
 
 set -euxo pipefail
 
+ARG1=${1:-}
+
 clear
-#rm -rf _build || true
+if [ $ARG1 == "--rst" ]; then
+    rm -rf _build || true
+fi
 cmake -G Ninja -B _build -S . -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build _build --config RelWithDebInfo
 ./_build/RelWithDebInfo/bin/luau_example example.lua
