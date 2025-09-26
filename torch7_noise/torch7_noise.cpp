b9ddc9da771e931eef73dc9be80292fcfb27b5fe
Mihaly Sisak <sisak.mihaly@gmail.com>
2025-09-26 12:58:45 +0200

    use cmake FetchContent, not git submodules for luau and FastNoise2

--- torch7_noise/torch7_noise.cpp
+++ torch7_noise/torch7_noise.cpp
@@ -40,9 +40,50 @@ static int torch_noise_simplex2D(lua_State *L)
     return 1;
 }
 
+static int torch_noise_simplex3D(lua_State *L)
+{
+    const char usage_str[] = "Usage: [res] torch.simplex3D([res,] scale, xstart, ystart, zstart, xsize, ysize, zsize, seed)\nError: %s\n";
+    int narg = lua_gettop(L);
+    if (((narg == 8) || (narg == 9)) == false) luaL_error(L, usage_str, "bad number of arguments");
+    int isnum = 0;
+    THFloatTensor* res = NULL;
+    if (narg == 9){
+        res = (THFloatTensor*)luaT_toudata(L, -9, "torch.FloatTensor");
+        if (res == NULL) luaL_error(L, usage_str, "argument res is not a torch.FloatTensor");
+    }
+    float scale = lua_tonumberx(L, -8, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument scale is not a number");
+    int xstart = lua_tointegerx(L, -7, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument xstart is not an integer");
+    int ystart = lua_tointegerx(L, -6, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument ystart is not an integer");
+    int zstart = lua_tointegerx(L, -5, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument zstart is not an integer");
+    int xsize = lua_tointegerx(L, -4, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument xsize is not an integer");
+    int ysize = lua_tointegerx(L, -3, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument ysize is not an integer");
+    int zsize = lua_tointegerx(L, -2, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument zsize is not an integer");
+    int seed = lua_tointegerx(L, -1, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument seed is not an integer");
+    if (res){
+        // resize guarantees res is 3d and contiguous
+        THFloatTensor_resize3d(res, xsize, ysize, zsize);
+    }
+    else {
+        res = THFloatTensor_newWithSize3d(xsize, ysize, zsize);
+    }
+    auto fn = FastNoise::New<FastNoise::Simplex>();
+    fn->SetScale(scale);
+    fn->GenUniformGrid3D(&res->storage->data[res->storageOffset], xstart, ystart, zstart, xsize, ysize, zsize, seed);
+    luaT_pushudata(L, res, "torch.FloatTensor");
+    return 1;
+}
+
 static int torch_noise_node2D(lua_State *L)
 {
-    const char usage_str[] = "Usage: [res] torch.node2D([res,] encoded node tree string, xstart, ystart, xsize, ysize, seed)\nError: %s\n";
+    const char usage_str[] = "Usage: [res] torch.node2D([res,] encoded_node_tree_string, xstart, ystart, xsize, ysize, seed)\nError: %s\n";
     int narg = lua_gettop(L);
     if (((narg == 6) || (narg == 7)) == false) luaL_error(L, usage_str, "bad number of arguments");
     int isnum = 0;
@@ -77,9 +118,52 @@ static int torch_noise_node2D(lua_State *L)
     return 1;
 }
 
+static int torch_noise_node3D(lua_State *L)
+{
+    const char usage_str[] = "Usage: [res] torch.node3D([res,] encoded_node_tree_string, xstart, ystart, zstart, xsize, ysize, zsize, seed)\nError: %s\n";
+    int narg = lua_gettop(L);
+    if (((narg == 8) || (narg == 9)) == false) luaL_error(L, usage_str, "bad number of arguments");
+    int isnum = 0;
+    THFloatTensor* res = NULL;
+    if (narg == 9){
+        res = (THFloatTensor*)luaT_toudata(L, -9, "torch.FloatTensor");
+        if (res == NULL) luaL_error(L, usage_str, "argument res is not a torch.FloatTensor");
+    }
+    const char* str = lua_tostring(L, -8);
+    if (str == NULL) luaL_error(L, usage_str, "argument str is not a string");
+    int xstart = lua_tointegerx(L, -7, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument xstart is not an integer");
+    int ystart = lua_tointegerx(L, -6, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument ystart is not an integer");
+    int zstart = lua_tointegerx(L, -5, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument zstart is not an integer");
+    int xsize = lua_tointegerx(L, -4, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument xsize is not an integer");
+    int ysize = lua_tointegerx(L, -3, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument ysize is not an integer");
+    int zsize = lua_tointegerx(L, -2, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument zsize is not an integer");
+    int seed = lua_tointegerx(L, -1, &isnum);
+    if (isnum == 0) luaL_error(L, usage_str, "argument seed is not an integer");
+    if (res){
+        // resize guarantees res is 3d and contiguous
+        THFloatTensor_resize3d(res, xsize, ysize, zsize);
+    }
+    else {
+        res = THFloatTensor_newWithSize3d(xsize, ysize, zsize);
+    }
+    auto fn = FastNoise::NewFromEncodedNodeTree(str);
+    if (bool(fn) == false) luaL_error(L, usage_str, "can not create noise generator from str");
+    fn->GenUniformGrid3D(&res->storage->data[res->storageOffset], xstart, ystart, zstart, xsize, ysize, zsize, seed);
+    luaT_pushudata(L, res, "torch.FloatTensor");
+    return 1;
+}
+
 static const struct luaL_Reg torch_noise__ [] = {
     {"simplex2D", torch_noise_simplex2D},
+    //{"simplex3D", torch_noise_simplex3D},
     {"node2D", torch_noise_node2D},
+    //{"node3D", torch_noise_node3D},
     {NULL, NULL}
 };
 
